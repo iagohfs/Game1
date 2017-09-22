2793833581374f1d1f1a168a5de543c0f5cc3dcf
Iagohfs <iago_henriquefs@hotmail.com>
2017-09-22 16:02:23 +0200

    Added a New class

--- Game/Game/Character.cs
+++ Game/Game/Character.cs
@@ -17,7 +17,6 @@ namespace Game
             this.Symbol = Symbol;
             Location = pos;
         }
-
         
 
     }
---
+++ Game/Game/ItemKey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class ItemKey : Entity
+    {
+        Coordinate Key = new Coordinate();
+
+        public ItemKey()
+        {
+            Location.posX = 7;
+            Location.posY = 7;
+
+            Symbol = '¥';
+        }
+
+        
+
+        
+
+    }
+}
